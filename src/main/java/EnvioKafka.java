a2f55627e3222fb2f73bda5a82d6eb3339a19034
Thiago Mendes <thiagoromendes@gmail.com>
2021-07-02 16:52:36 -0300

    first commit

---
+++ src/main/java/EnvioKafka.java
@@ -0,0 +1,25 @@
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class EnvioKafka {
+	
+	public static void main(String[] args) {
+		
+		Properties properties = new Properties();
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+		
+		ProducerRecord<String, String> record = new ProducerRecord<String, String>("java", "Olá Mundo!");
+		
+		producer.send(record);
+		producer.close();
+	}
+
+}
